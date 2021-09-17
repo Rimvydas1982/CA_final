fc205944d6d69aeb786286e3eca9d9de4de89ff5
rimvydas1982 <rimwe22@gmail.com>
2021-09-17 14:28:44 +0300

    MD added

---
+++ 1_frontend/src/components/UpdateUser.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const UpdateUser = () => {
+  //Hooks
+  //local
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [email, setEmail] = useState('');
+  return (
+    <div>
+      <h2>Atnaujinti vartotojo duomenis</h2>
+
+      <form>
+        <div>
+          <label htmlFor='name'>Vardas</label>
+          <input
+            type='text'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor='age'>Amzius</label>
+          <input
+            type='number'
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label htmlFor='model'>El.pastas</label>
+          <input
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <input type='submit' value='Atnaujinti' />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateUser;
